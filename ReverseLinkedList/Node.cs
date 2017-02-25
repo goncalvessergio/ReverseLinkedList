e9b1a3b3a68ab7e49be167977d26f89c18e16b95
Sérgio Gonçalves <sergio.goncalves@outlook.pt>
2017-02-25 23:24:41 +0000

    First commit

---
+++ ReverseLinkedList/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace ReverseLinkedList
+{
+    class Node
+    {
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+    }
+}
--- ReverseLinkedList/Program.cs
+++ ReverseLinkedList/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReverseLinkedList
 {
@@ -10,6 +6,56 @@ namespace ReverseLinkedList
     {
         static void Main(string[] args)
         {
+            var nodeFive = new Node(value: 5);
+            var nodeFour = new Node(value: 4) { Next = nodeFive };
+            var nodeThree = new Node(value: 3) { Next = nodeFour };
+            var nodeTwo = new Node(value: 2) { Next = nodeThree };
+            var nodeOne = new Node(value: 1) { Next = nodeTwo };
+            var rootNode = new Node(value: 0) { Next = nodeOne };
+            PrintLinkedList(rootNode);
+
+            var rootNodeReversed = ReverseListRec(rootNode, previous: null);
+            PrintLinkedList(rootNodeReversed);
+
+            Console.ReadKey();
+        }
+
+        // Recursive
+        static Node ReverseListRec(Node node, Node previous)
+        {
+            var next = node.Next;
+            node.Next = previous;
+
+            return next == null ? node : ReverseListRec(next, node);
+        }
+
+        // Iterative
+        static Node ReverseListIt(Node rootNode)
+        {
+            Node next;
+            Node prev = null;
+
+            var node = rootNode;
+            while (node != null)
+            {
+                next = node.Next;
+                node.Next = prev;
+                prev = node;
+
+                node = next;
+            }
+
+            return prev;
+        }
+
+        static void PrintLinkedList(Node rootNode)
+        {
+            var node = rootNode;
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }
